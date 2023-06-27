c72ab23edb114010840f0596b2d8e2d4b73b763d
Dan Pankratev <thepoddubstep@gmail.com>
2023-06-27 23:04:34 +0300

    Add support for fill

--- lib/magic_report/general_report.rb
+++ lib/magic_report/general_report.rb
@@ -2,17 +2,18 @@
 
 module MagicReport
   class GeneralReport
-    attr_reader :models, :report_klass, :csv
+    attr_reader :models, :report_klass, :csv, :fill
 
-    def initialize(input, report_klass)
+    def initialize(input, report_klass, options = {})
       @models = input.is_a?(Enumerable) ? input : [input]
       @report_klass = report_klass
       @csv = ::MagicReport::Report::Csv.new(self)
+      @fill = options[:fill] || false
     end
 
     def generate
       models.each.with_index do |model, index|
-        report = report_klass.new(model)
+        report = report_klass.new(model, fill)
 
         csv.add_headings(report) if index.zero?
         report.rows.each { |row| csv.add_row(row) }
